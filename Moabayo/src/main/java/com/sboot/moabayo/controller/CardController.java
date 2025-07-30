1d54af4a5468ed584e4f0a56a2e1c8c8f3ea2af7
sung-min <dbs0877@gmail.com>
2025-07-30 15:31:41 +0900

    가상 카드 시스템 테스트용

---
+++ Moabayo/src/main/java/com/sboot/moabayo/controller/CardController.java
@@ -0,0 +1,25 @@
+package com.sboot.moabayo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+public class CardController {
+
+	
+	@GetMapping("/cards")
+	public String showCardList(Model model) {
+//	    List<CardDTO> cardList = cardService.getMyCards(); // 카드 리스트 조회
+//	    model.addAttribute("cardList", cardList);
+	    return "cardList"; // cardList.html 렌더링
+	}
+	
+	@GetMapping("/recommendcards")
+	public String showRecommendList(Model model) {
+//	    List<CardDTO> cardList = cardService.getMyCards(); // 카드 리스트 조회
+//	    model.addAttribute("cardList", cardList);
+	    return "card-recommendation"; // cardList.html 렌더링
+	}
+}
