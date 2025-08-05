c3ddf7ad0174189c13cfcd55ad7e59d02edf02f2
KHS <ddw1079@naver.com>
2025-08-05 10:36:26 +0900

    Eureka Client - Bank, Card, Admin 프로젝트 각 생성

--- Moabayo_Client_Admin/src/main/java/com/sboot/moabayo/MoabayoClientAdminApplication.java
+++ Moabayo_Client_Admin/src/main/java/com/sboot/moabayo/MoabayoClientAdminApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MoabayoApplication {
+public class MoabayoClientAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoabayoApplication.class, args);
+		SpringApplication.run(MoabayoClientAdminApplication.class, args);
 	}
 
 }
--- Moabayo_Client_Bank/src/main/java/com/sboot/moabayo/MoabayoClientBankApplication.java
+++ Moabayo_Client_Bank/src/main/java/com/sboot/moabayo/MoabayoClientBankApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MoabayoApplication {
+public class MoabayoClientBankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoabayoApplication.class, args);
+		SpringApplication.run(MoabayoClientBankApplication.class, args);
 	}
 
 }
