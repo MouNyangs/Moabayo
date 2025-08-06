88ebfeb6f9ddf151eae8674b8aa0a34523e5ace1
sung-min <dbs0877@gmail.com>
2025-08-06 12:31:06 +0900

    openfeign작업중 Eureka 오류수정

--- Moabayo_Client_Admin/src/main/java/com/sboot/moabayo/MoabayoClientAdminApplication.java
+++ Moabayo_Client_Admin/src/main/java/com/sboot/moabayo/MoabayoClientAdminApplication.java
@@ -3,13 +3,14 @@ package com.sboot.moabayo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient           // Eureka 클라이언트로 등록
+@EnableFeignClients(basePackages = "com.sboot.moabayo.feign") // Feign 인터페이스 패키지 경로 지정 (선택)
 public class MoabayoClientAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoabayoClientAdminApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MoabayoClientAdminApplication.class, args);
+    }
 }
