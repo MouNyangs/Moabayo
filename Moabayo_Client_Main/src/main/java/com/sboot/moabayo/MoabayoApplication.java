89b4a04a92e31531b0ff0725b8977f67fae543f0
yoonjae0825 <qortpaos@naver.com>
2025-08-06 12:31:48 +0900

    openfegin시행

--- Moabayo_Client_Main/src/main/java/com/sboot/moabayo/MoabayoApplication.java
+++ Moabayo_Client_Main/src/main/java/com/sboot/moabayo/MoabayoApplication.java
@@ -3,9 +3,10 @@ package com.sboot.moabayo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.sboot.moabayo.feign")
 public class MoabayoApplication {
 
 	public static void main(String[] args) {
