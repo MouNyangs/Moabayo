f3fd93a0d48d5ef9d70b898590c9593690bdce88
KHS <ddw1079@naver.com>
2025-08-07 14:44:00 +0900

    나머지 4개 프로젝트 전체 Swagger UI 접속 기능 설정 Swagger UI 접속 위치: localhost:[포트번호]/docs

--- Moabayo_Client_LoginService/src/main/java/com/sboot/moabayo/swagger/SwaggerConfig.java
+++ Moabayo_Client_LoginService/src/main/java/com/sboot/moabayo/swagger/SwaggerConfig.java
@@ -11,8 +11,8 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("모아봐요 모으냥즈 - Main API 문서")
+                        .title("모아봐요 모으냥즈 - LoginService API 문서")
                         .version("1.0")
-                        .description("모으냥즈 Main 프로젝트 Swagger 문서입니다."));
+                        .description("모으냥즈 LoginService 프로젝트 Swagger 문서입니다."));
     }
 }
