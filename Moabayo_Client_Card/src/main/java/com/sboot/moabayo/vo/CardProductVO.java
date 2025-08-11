87a46dce73b3fc7352df07dd18e08e1a2cc72ec6
sung-min <dbs0877@gmail.com>
2025-08-11 13:01:22 +0900

    Card 서버 VO mapper cotroller 추가 및 수정

---
+++ Moabayo_Client_Card/src/main/java/com/sboot/moabayo/vo/CardProductVO.java
@@ -0,0 +1,18 @@
+package com.sboot.moabayo.vo;
+
+import lombok.Data;
+
+@Data
+public class CardProductVO {
+
+	private String cardId;
+	private String img;
+	private String name;
+	private String brand;
+	private String description;
+	private String category;
+	private String benefits;
+	private String interest;
+	private String type;
+	
+}
