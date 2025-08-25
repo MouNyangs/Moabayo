2980bcda146722f646aab7aa8f38c95fef0b2084
KHS <ddw1079@naver.com>
2025-08-25 14:29:24 +0900

    LoginService 에 패스워드 체크 백엔드 작업 완료

--- Moabayo_Client_LoginService/src/main/java/com/sboot/moabayo/controller/UserDetailController.java
+++ Moabayo_Client_LoginService/src/main/java/com/sboot/moabayo/controller/UserDetailController.java
@@ -2,13 +2,17 @@ package com.sboot.moabayo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sboot.moabayo.service.LoginService;
 import com.sboot.moabayo.service.UserDetailService;
 import com.sboot.moabayo.vo.UserDetailVO;
+import com.sboot.moabayo.vo.UserVO;
 
 @RestController
 @RequestMapping("/api/userdetail")
@@ -27,6 +31,31 @@ public class UserDetailController {
                     .body("User not found: " + loginId);
         }
         return ResponseEntity.ok(vo);
+    }
+    
+    private PasswordEncoder pwEncoder;
+    private LoginService loginServ;
+    
+    @GetMapping("/pwcheck")
+    public ResponseEntity<?> validateUserPassword(
+    		@RequestParam String loginId,
+    		@RequestParam String insertedPw
+    		) {
+    	// 1. Request로 날라온 로그인 ID가 있는지 확인.
+        if (loginId == null || loginId.isBlank()) {
+            return ResponseEntity.badRequest().body("loginId is required");
+        }
+        // 2. loginId로 유저 조회
+        UserVO user = loginServ.login(loginId);
+        
+        // 3. Request로 날라온 비밀번호와 유저의 비밀번호를 매칭
+        //  - 비밀번호가 맞으면 ok 와 함께 "비밀번호 일치" 리턴
+        //	- 비밀번호가 틀리면 ok 와 함께 "비밀번호 불일치" 리턴
+        if (user != null && pwEncoder.matches(insertedPw, user.getPassword())) {
+        	return ResponseEntity.ok("비밀번호 일치");
+        } else {
+        	return ResponseEntity.ok("비밀번호 불일치");
+        }
     }
 	
 }
